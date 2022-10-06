f41f09c6076e0aac2ceab665484ca12c6c1ef26d
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-07 00:30:52 +0300

    isomorphic strings problem is solved

---
+++ Level1/day2_string/isomorphic_strings.py
@@ -0,0 +1,38 @@
+"""
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to
+get t.
+
+All occurrences of a character must be replaced with another character while
+preserving the order of characters. No two characters may map to the same
+character, but a character may map to itself.
+
+
+
+Example 1:
+
+>>> Solution().isIsomorphic("egg","add")
+True
+
+Example 2:
+
+>>> Solution().isIsomorphic("foo","bar")
+False
+
+Example 3:
+
+>>> Solution().isIsomorphic("paper","title")
+True
+"""
+import doctest
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t) or len(set(s)) != len(set(t)):
+            return False
+        return True
+
+
+doctest.testmod()
