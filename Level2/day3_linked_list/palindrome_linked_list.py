e85d50142ec0488ba05fb6579fdc10a5053e021a
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-11-01 12:26:25 +0300

    palindrome linked list problem is solved

---
+++ Level2/day3_linked_list/palindrome_linked_list.py
@@ -0,0 +1,41 @@
+"""
+Given the head of a singly linked list, return true if it is a palindrome or
+false otherwise.
+
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+
+Input: head = [1,2]
+Output: false
+
+"""
+from typing import Optional
+
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        nums = []
+
+        while head:
+            nums.appen(head.val)
+            head = head.next
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] != nums[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
