4f2e39d5279e6285b96f083731dba1ff47628768
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-11-01 11:36:29 +0300

    longest prefix doctests passed

--- Level2/day2_string/longest_common_prefix.py
+++ Level2/day2_string/longest_common_prefix.py
@@ -9,12 +9,12 @@ If there is no common prefix, return an empty string "".
 Example 1:
 
 >>> Solution().longestCommonPrefix(["flower","flow","flight"])
-"fl"
+'fl'
 
 Example 2:
 
 >>> Solution().longestCommonPrefix(["dog","racecar","car"])
-""
+''
 
 Explanation: There is no common prefix among the input strings.
 """
@@ -26,12 +26,16 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         right = 1
         prefix = strs[0][0:right]
-        while right < len(strs):
-            for string in strs[1:]:
-                if string[0:right] != prefix:
+        while right < len(strs) - 1:
+
+            for string in strs:
+                if string[0] != strs[0][0]:
                     return ""
+                if string[0:right] != prefix:
+                    return prefix[:-1]
                 else:
                     right += 1
+                    prefix = strs[0][0:right]
         return prefix
 
 
